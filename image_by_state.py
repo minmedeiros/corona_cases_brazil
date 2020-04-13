c30a8ba29e690e9679d43c6ca92152b575215df4
Yasmin Medeiros <aminmedeiros@hotmail.com>
2020-04-13 00:04:15 -0300

    updates til 12 of april + deaths of main states csv

--- image_by_state.py
+++ image_by_state.py
@@ -10,7 +10,7 @@ conn = sqlite3.connect('corona_per_state.db')
 cursor = conn.cursor()
 
 # Select all rows and columns from sheet "CORONA_CASES", there is also "CORONA_DEATHS"
-cursor.execute("SELECT * FROM CORONA_DEATHS")
+cursor.execute("SELECT * FROM CORONA_CASES")
 
 # Get name of the fields... you should expect "Date", and then each state abbreviation alphabetically)
 columns = [description[0] for description in cursor.description]
@@ -43,7 +43,7 @@ for col in columns:
     plt.plot(datas.index,datas[col])
     plt.gcf().autofmt_xdate()
     plt.grid(True)
-    plt.title('Coronavirus Deaths in ' + col)
+    plt.title('Coronavirus Cases in ' + col)
     #plt.show()
-    plt.savefig(col + '_deaths.png', dpi=200)
+    plt.savefig(col + '_cases.png', dpi=200)
     plt.close()
